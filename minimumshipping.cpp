097735771ab74c1baaee4c705f66db78df64d416
harshalDev <harshalwarade344@gmail.com>
2023-02-22 16:53:34 +0530

    commited  changes

---
+++ minimumshipping.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int returnMinimum(int arr[], int size, int days) {
+    vector<int> addCollection = {};
+    for(int i = 0; i < size; i = i+2) {
+        int first = arr[i];
+        int second = arr[i+1];
+        int adder = first + second;
+        addCollection.push_back(adder);
+    }
+    
+    cout << ;
+}
+
+int main()
+{
+    int arr[6] = {3,2,2,4,1,4};
+    returnMinimum(arr, 6, 3);
+    return 0;
+}
