15ea36e1d89a7af02684f1bebac3d36233d799ff
harshalDev <harshalwarade344@gmail.com>
2023-04-12 01:49:18 +0530

    added comment!

--- linkedList/prac.cpp
+++ linkedList/prac.cpp
@@ -14,9 +14,36 @@ class Node {
     }
 };
 
+void insert(Node* &head, int data) {
+    Node* temp = new Node(data);
+    temp -> next = head;
+    head = temp;
+}
+
+void print(Node* &head) {
+    Node* temp = head;
+    while(temp != NULL) {
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+    cout << endl;
+}
+
 int main() {
 
-    
+    Node* node1 = new Node(40);
+
+    Node* head = node1;
+
+    print(head);
+    insert(head, 75);
+    print(head);
+    insert(head, 65);
+    print(head);
+    insert(head, 13);
+    print(head);
+    insert(head, 98);
+    print(head);
 
     return 0;
 }
